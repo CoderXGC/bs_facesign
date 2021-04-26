d5c5557a7051505c6bc2beec90bf7a274ed058a6
CoderXGC <xgc@tom.com>
2021-04-26 22:47:09 +0800

    修改登录日志UI完成修改。

--- BS_FS/Form_People_Signlog.Designer.cs
+++ BS_FS/Form_People_Signlog.Designer.cs
@@ -35,81 +35,20 @@ namespace BS_FS
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.signouttime = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.uiSymbolButton1 = new Sunny.UI.UISymbolButton();
             this.uiSymbolButton2 = new Sunny.UI.UISymbolButton();
             this.uiDatePicker1 = new Sunny.UI.UIDatePicker();
             this.uiStyleManager1 = new Sunny.UI.UIStyleManager(this.components);
             this.uiDataGridView1 = new Sunny.UI.UIDataGridView();
-            this.dataSet1 = new System.Data.DataSet();
             this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.signintime = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.signout = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dataSet1 = new System.Data.DataSet();
             ((System.ComponentModel.ISupportInitialize)(this.uiDataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToOrderColumns = true;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3,
-            this.signouttime});
-            this.dataGridView1.EnableHeadersVisualStyles = false;
-            this.dataGridView1.Location = new System.Drawing.Point(416, 15);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(513, 268);
-            this.dataGridView1.TabIndex = 9;
-            // 
-            // Column1
-            // 
-            this.Column1.DataPropertyName = "id";
-            this.Column1.HeaderText = "员工ID";
-            this.Column1.MinimumWidth = 6;
-            this.Column1.Name = "Column1";
-            this.Column1.ReadOnly = true;
-            this.Column1.Width = 125;
-            // 
-            // Column2
-            // 
-            this.Column2.DataPropertyName = "name";
-            this.Column2.HeaderText = "员工姓名";
-            this.Column2.MinimumWidth = 6;
-            this.Column2.Name = "Column2";
-            this.Column2.ReadOnly = true;
-            this.Column2.Width = 125;
-            // 
-            // Column3
-            // 
-            this.Column3.DataPropertyName = "signintime";
-            this.Column3.HeaderText = "签到时间";
-            this.Column3.MinimumWidth = 6;
-            this.Column3.Name = "Column3";
-            this.Column3.ReadOnly = true;
-            this.Column3.Width = 125;
-            // 
-            // signouttime
-            // 
-            this.signouttime.HeaderText = "签退时间";
-            this.signouttime.MinimumWidth = 6;
-            this.signouttime.Name = "signouttime";
-            this.signouttime.Width = 125;
             // 
             // label1
             // 
@@ -120,15 +59,6 @@ namespace BS_FS
             this.label1.Size = new System.Drawing.Size(157, 15);
             this.label1.TabIndex = 8;
             this.label1.Text = "请选择查看签到日期：";
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(513, 305);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(265, 25);
-            this.dateTimePicker1.TabIndex = 7;
-            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // uiSymbolButton1
             // 
@@ -172,6 +102,7 @@ namespace BS_FS
             this.uiDatePicker1.SymbolDropDown = 61555;
             this.uiDatePicker1.SymbolNormal = 61555;
             this.uiDatePicker1.TabIndex = 14;
+            this.uiDatePicker1.Text = "2021-04-25";
             this.uiDatePicker1.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
             this.uiDatePicker1.Value = new System.DateTime(2021, 4, 25, 22, 41, 10, 0);
             this.uiDatePicker1.ValueChanged += new Sunny.UI.UIDatePicker.OnDateTimeChanged(this.uiDatePicker1_ValueChanged);
@@ -209,7 +140,7 @@ namespace BS_FS
             this.uiDataGridView1.EnableHeadersVisualStyles = false;
             this.uiDataGridView1.Font = new System.Drawing.Font("微软雅黑", 12F);
             this.uiDataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiDataGridView1.Location = new System.Drawing.Point(288, 11);
+            this.uiDataGridView1.Location = new System.Drawing.Point(309, 11);
             this.uiDataGridView1.Name = "uiDataGridView1";
             dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
@@ -227,10 +158,6 @@ namespace BS_FS
             this.uiDataGridView1.ShowGridLine = true;
             this.uiDataGridView1.Size = new System.Drawing.Size(704, 363);
             this.uiDataGridView1.TabIndex = 15;
-            // 
-            // dataSet1
-            // 
-            this.dataSet1.DataSetName = "NewDataSet";
             // 
             // id
             // 
@@ -260,6 +187,10 @@ namespace BS_FS
             this.signout.Name = "signout";
             this.signout.Width = 200;
             // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
             // Form_People_Signlog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
@@ -269,13 +200,10 @@ namespace BS_FS
             this.Controls.Add(this.uiDatePicker1);
             this.Controls.Add(this.uiSymbolButton2);
             this.Controls.Add(this.uiSymbolButton1);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dateTimePicker1);
             this.Name = "Form_People_Signlog";
             this.Text = "Form_People_Signlog";
             this.Load += new System.EventHandler(this.Form_People_Signlog_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.uiDataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
             this.ResumeLayout(false);
@@ -284,13 +212,7 @@ namespace BS_FS
         }
 
         #endregion
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn signouttime;
         private Sunny.UI.UISymbolButton uiSymbolButton1;
         private Sunny.UI.UISymbolButton uiSymbolButton2;
         private Sunny.UI.UIDatePicker uiDatePicker1;
--- BS_FS/Form_People_Signlog.cs
+++ BS_FS/Form_People_Signlog.cs
@@ -26,97 +26,16 @@ namespace BS_FS
 
         private void Form_People_Signlog_Load(object sender, EventArgs e)
         {
-           
+            uiDatePicker1.Value = DateTime.Now;
             uiDataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;//设置列标题文字
             uiDataGridView1.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;//设置单元格内容居中
             uiDataGridView1.AllowUserToResizeColumns = false;//禁止用户改变DataGridView1的所有列的列宽
             uiDataGridView1.AllowUserToResizeRows = false;//禁止用户改变DataGridView1の所有行的行高
             uiDataGridView1.RowHeadersVisible = false;//第一列空白
-            dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;//设置列标题文字居中
-            dataGridView1.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;//设置单元格内容居中
-            dataGridView1.AllowUserToResizeColumns = false;//禁止用户改变DataGridView1的所有列的列宽
-            dataGridView1.AllowUserToResizeRows = false;//禁止用户改变DataGridView1の所有行的行高
-            dataGridView1.RowHeadersVisible = false;//第一列空白
             uiSymbolButton1.Enabled = false;//禁止导出按钮
            
         }
 
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
-        {
-            while (this.dataGridView1.Rows.Count!= 0)
-            {
-
-                this.dataGridView1.Rows.RemoveAt(0);
-            }
-
-            dateTimePicker1.Value.ToString("yyyy-MM-dd");
-            int compNum = DateTime.Compare(dateTimePicker1.Value.Date, DateTime.Now);
-            if (compNum > 0)
-            {
-                MessageBox.Show("查询错误，请选择比当前日期小的时间。");
-                uiSymbolButton1.Enabled = false;
-            }
-            else
-            {
-                dataGridView1.ReadOnly = true;
-                Net n = new Net();
-
-                //这个需要引入Newtonsoft.Json这个DLL并using
-                //传入实体类还有需要解析的JSON字符串这样就OK了。然后就可以通过实体类使用数据了。
-                JsonBean rt = JsonConvert.DeserializeObject<JsonBean>(n.Findsign(this.Text, this.Text + dateTimePicker1.Value.ToString("yyyy-MM-dd")));
-                if (rt.code.ToString() == "200")
-                {
-
-                    this.dataGridView1.Rows.Add(rt.data.user_id, "徐广超", rt.data.signintime, rt.data.signouttime);
-                    //     DataGridView1.Columns[0].
-                    //   dataGridView1.DataSource = rt.data.signintime;
-                    uiSymbolButton1.Enabled = true;
-
-                }
-                else if (rt.code.ToString() == "-1")
-                {
-                    MessageBox.Show(dateTimePicker1.Value.ToString("yyyy-MM-dd") + "当天您没有签到哦");
-                    uiSymbolButton1.Enabled = false;
-
-                }
-                else if (rt.code.ToString() == "404")
-                {
-                    ShowWarningTip(rt.message);
-
-                }
-                else if (rt.code.ToString() == "100")
-                {
-                    ShowWarningTip(rt.message);
-
-                }
-                else if (rt.code.ToString() == "1000")
-                {
-                    ShowWarningTip(rt.message);
-
-                }
-                /*    DataMysql data = new DataMysql();
-                    data.dataCon();
-                    //查询数据库
-                    string cmdStr = "Select signlog.id,user.name,signlog.signintime from user,signlog where daytime='" + dateTimePicker1.Value.ToString("yyyy-MM-dd") + "' and signlog.id=user.id ";
-                    MySqlConnection con = new MySqlConnection("server=116.62.110.115;port=3306;user=facesign;password=99d44172db8d6d58;database=facesign");
-                    MySqlCommand sqlCmd = new MySqlCommand(cmdStr, con);
-                    MySqlDataAdapter sda = new MySqlDataAdapter(sqlCmd);
-                    DataSet ds = new DataSet();
-                    sda.Fill(ds, "sign");
-                    ds = data.getDataSet(cmdStr);
-                    if (ds.Tables[0].Rows.Count == 0)
-                    {
-                        MessageBox.Show(dateTimePicker1.Value.ToString("yyyy-MM-dd") + "当天没有人员签到哦");
-                        button1.Enabled = false;
-                    }
-                    else
-                    {
-                        dataGridView1.DataSource = ds.Tables[0];
-                        button1.Enabled = true;
-                    }*/
-
-            }
-        }
 
 
         //引用登录提示框架弹出气泡
@@ -137,7 +56,7 @@ namespace BS_FS
             saveFileDialog.Title = "导出Excel文件到";
 
             DateTime now = DateTime.Now;
-            saveFileDialog.FileName = dateTimePicker1.Value.ToString("yyyy-MM-dd") + "人员签到情况表";
+            saveFileDialog.FileName = uiDatePicker1.Value.ToString("yyyy-MM-dd") + "人员签到情况表";
             saveFileDialog.ShowDialog();
 
             Stream myStream = saveFileDialog.OpenFile();
@@ -146,26 +65,26 @@ namespace BS_FS
             try
             {
                 //写标题     
-                for (int i = 0; i < this.dataGridView1.ColumnCount; i++)
+                for (int i = 0; i < this.uiDataGridView1.ColumnCount; i++)
                 {
                     if (i > 0)
                     {
                         str += "\t";
                     }
-                    str += this.dataGridView1.Columns[i].HeaderText;
+                    str += this.uiDataGridView1.Columns[i].HeaderText;
                 }
                 sw.WriteLine(str);
                 //写内容   
-                for (int j = 0; j < this.dataGridView1.Rows.Count; j++)
+                for (int j = 0; j < this.uiDataGridView1.Rows.Count; j++)
                 {
                     string tempStr = "";
-                    for (int k = 0; k < this.dataGridView1.Columns.Count; k++)
+                    for (int k = 0; k < this.uiDataGridView1.Columns.Count; k++)
                     {
                         if (k > 0)
                         {
                             tempStr += "\t";
                         }
-                        tempStr += this.dataGridView1.Rows[j].Cells[k].Value.ToString();
+                        tempStr += this.uiDataGridView1.Rows[j].Cells[k].Value.ToString();
                     }
                     sw.WriteLine(tempStr);
                 }
@@ -218,7 +137,7 @@ namespace BS_FS
                 {
 
                     this.uiDataGridView1.Rows.Add(rt.data.user_id, "徐广超", rt.data.signintime, rt.data.signouttime);
-                    this.dataGridView1.Rows.Add(rt.data.user_id, "徐广超", rt.data.signintime, rt.data.signouttime);
+           //         this.dataGridView1.Rows.Add(rt.data.user_id, "徐广超", rt.data.signintime, rt.data.signouttime);
                     //     DataGridView1.Columns[0].
                     //   dataGridView1.DataSource = rt.data.signintime;
                     uiSymbolButton1.Enabled = true;
